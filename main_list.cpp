7d2f682448e4337515e071fda4ed4709a0530a44
michaellimair <michaellimbong@gmail.com>
2019-10-31 22:12:11 +0800

    add driver programs for stack and rename

--- main_list.cpp
+++ main_list.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include "List.cpp"
-
+#include "List.cpp" 
 using namespace std;
 
 int main() {
---
+++ main_stack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "stack.cpp"
+
+using std::endl;
+using std::cout;
+
+int main() { 
+    MyStack *s = new MyStack();
+
+    s->push(1);
+    s->push(2);
+    s->push(3);
+
+    for (int i = 1; i <= 3; i++) {
+        cout << s->top() << endl;
+        s->pop();
+    }
+
+    return 0;
+}
